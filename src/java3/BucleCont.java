e1749692368d26d0f6c7b984eca12772e3cfb7b9
César Pinto <63161873+csrap@users.noreply.github.com>
2022-01-08 14:29:52 +0100

    Java Básico 3

---
+++ src/java3/BucleCont.java
@@ -0,0 +1,13 @@
+package java3;
+
+public class BucleCont {
+
+    public static void main(String[] args) {
+        String [] names = { "Juan", "Pedro", "Maria", "Jose", "Jaime"};
+
+        for( String name:names) {
+            System.out.print(name + " ");
+        }
+    }
+
+}
